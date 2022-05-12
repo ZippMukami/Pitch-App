83102db5b80b9aa8c2274f782c0fbde8fbfa2469
ZippMukami <zimukami@gmail.com>
2022-05-12 03:51:38 +0300

    adds page titles.

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,4 @@
+from turtle import title
 from flask import Flask, render_template
 app = Flask (__name__)
 
@@ -24,7 +25,7 @@ def name():
 
 @app.route("/about")
 def about():
-    return render_template('about.html')   
+    return render_template('about.html', title = 'About')   
 
 if __name__=='__main__':
     app.run(debug=True)
