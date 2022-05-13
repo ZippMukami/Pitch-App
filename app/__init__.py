1f89be04aab990d586873d69f073549a7bbe6fec
ZippMukami <zimukami@gmail.com>
2022-05-13 05:52:32 +0300

    adds form validation and allows to get data from user.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,5 @@
 from turtle import title
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 app = Flask (__name__)
 
@@ -23,7 +23,7 @@ pitches = [
 
 @app.route("/")
 @app.route("/home")
-def name():
+def home():
     return render_template('home.html', pitches=pitches)
 
 
@@ -31,9 +31,15 @@ def name():
 def about():
     return render_template('about.html', title = 'About')   
 
-@app.route("/register")
+@app.route("/register", methods= ['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+
+
+
     return render_template('register.html', title = 'Register', form=form)
 
 @app.route("/login")
