647a24393a7d7f9e6add8df12b4b4b42c64c1fd2
ZippMukami <zimukami@gmail.com>
2022-05-13 04:07:40 +0300

    adds registration and login routes.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
+from turtle import title
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 app = Flask (__name__)
 
-app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = '594910733c8c780091c2cf7dd0eb0bed'
 
 pitches = [
     {
@@ -24,9 +26,20 @@ pitches = [
 def name():
     return render_template('home.html', pitches=pitches)
 
+
 @app.route("/about")
 def about():
     return render_template('about.html', title = 'About')   
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title = 'Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form=form)    
 
 if __name__=='__main__':
     app.run(debug=True)
