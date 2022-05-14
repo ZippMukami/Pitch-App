b0c6fea6e2c92705bbe650783e56779933adf89d
ZippMukami <zimukami@gmail.com>
2022-05-14 16:14:55 +0300

    adds user and pitch classes.

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,40 @@
+from datetime import datetime
+from calendar import c
+from email.policy import default
 from turtle import title
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
-app = Flask (__name__)
 
+app = Flask (__name__)
 app.config['SECRET_KEY'] = '594910733c8c780091c2cf7dd0eb0bed'
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique = True, nullable = False)
+    email = db.Column(db.String(120), unique = True, nullable = False)
+    image_file = db.Column(db.String(20), nullable = False, default= 'default.jpg')
+    password = db.Column(db.String(60), nullable = False)
+    pitches = db.relationship('Pitch', backref = 'author', lazy = True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Pitch(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100),nullable = False)
+    date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    content = db.Column(db.Text, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = false)
+
+    def __repr__(self):
+        return f"Pitch('{self.title}', '{self.date_posted}')"
+
+
+ 
 
 pitches = [
     {
