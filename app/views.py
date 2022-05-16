a3def257914ea83d95e151555e7cc88033021b38
ZippMukami <zimukami@gmail.com>
2022-05-16 09:24:27 +0300

    adds pitch id and title.

--- app/views.py
+++ app/views.py
@@ -101,3 +101,8 @@ def new_pitch():
         flash('Your pitch has been created', 'success')
         return redirect(url_for('home'))
     return render_template('create_pitch.html', title='New Pitch', form=form)    
+
+@app.route("/pitch/<int:pitch_id>" )
+def pitch(pitch_id):
+    pitch = Pitch.query.get_or_404(pitch_id)
+    return render_template('pitch.html', title = pitch.title, pitch =pitch)
